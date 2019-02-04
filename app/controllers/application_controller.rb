72d0f54cd03f386bf39184097d4aa006b77708cf
Brian Gates <briandotgates@gmail.com>
2019-02-03 22:42:37 -0500

    tired

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,9 @@ private
     begin
       slack_request.verify!
     rescue
-
-        digest = OpenSSL::Digest::SHA256.new
-        signature_basestring = [slack_request.version, slack_request.timestamp, slack_request.body].join(':')
-        #hex_hash = OpenSSL::HMAC.hexdigest(digest, Slack::Events.config.signing_secret, signature_basestring)
-        #computed_signature = [slack_request.version, hex_hash].join('=')
       render json: {
         response_type: "ephemeral",
-        text: "Sorry, that didn't work. Please try again. signature_basestring:#{signature_basestring}, #{Slack::Events.config.signing_secret},  #{slack_request.signature}"
+        text: "Sorry, that didn't work. Please try again. #{Slack::Events.config.signing_secret},  #{slack_request.signature}"
       }
     end
   end
