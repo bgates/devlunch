f74fa28e0297bfc9021253a4beca23506c3f7ebc
Brian Gates <briandotgates@gmail.com>
2019-02-03 21:57:04 -0500

    so much failure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ private
         digest = OpenSSL::Digest::SHA256.new
         signature_basestring = [slack_request.version, slack_request.timestamp, slack_request.body].join(':')
         hex_hash = OpenSSL::HMAC.hexdigest(digest, Slack::Events.config.signing_secret, signature_basestring)
-        computed_signature = [version, hex_hash].join('=')
+        computed_signature = [slack_request.version, hex_hash].join('=')
       render json: {
         response_type: "ephemeral",
         text: "Sorry, that didn't work. Please try again. signature_basestring:#{signature_basestring}, computed_signature:#{computed_signature}, signature: #{slack_request.signature}"
