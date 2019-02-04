73a49f564fcf7dfc59091f077e5d135b4acf4f7e
Brian Gates <briandotgates@gmail.com>
2019-02-03 21:21:55 -0500

    slack still erroring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,9 @@ private
     begin
       slack_request.verify!
     rescue
-      logger.info slack_request.inspect
       render json: {
         response_type: "ephemeral",
-        text: "Sorry, that didn't work. Please try again. #{slack_request.inspect}"
+        text: "Sorry, that didn't work. Please try again. #{request.headers.inspect}"
       }
     end
   end
