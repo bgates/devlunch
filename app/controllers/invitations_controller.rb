8cd9e92ce23e957e8def2911e7324e5e64bdc9b6
Brian Gates <briandotgates@gmail.com>
2019-02-03 20:46:32 -0500

    csrf still

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,15 +1,6 @@
 class InvitationsController < ApplicationController
 
   def create
-    slack_request = Slack::Events::Request.new(request)
-    begin
-      slack_request.verify!
-      render json: { response_type: "in_channel" }
-    rescue
-      render json: {
-        response_type: "ephemeral",
-        text: "Sorry, that didn't work. Please try again."
-      }
-    end
+    render json: { response_type: "in_channel" }
   end
 end
