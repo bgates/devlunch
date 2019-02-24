f933e1537fbab4a6d71e62b1516243bb0353e654
Brian Gates <briandotgates@gmail.com>
2019-02-24 17:53:06 -0500

    try to solve cors

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,4 +1,6 @@
 class InvitationsController < ApplicationController
+before_action :cors_preflight_check, only: :index
+after_action :cors_set_access_control_headers, only: :index
 
   def create
     Invitation.create_from_yelp_data(params[:text], params[:user_id])
@@ -7,5 +9,21 @@ class InvitationsController < ApplicationController
 
   def index 
     @invitations = Invitation.all
+  end
+
+  private 
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == "OPTIONS"
+      cors_set_access_control_headers
+      render :text => '', :content_type => 'text/plain'
+    end
   end
 end
