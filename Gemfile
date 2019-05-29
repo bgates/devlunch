cf64b20f0ad58f575a6ac1b9286defc47388f858
Brian Gates <briandotgates@gmail.com>
2019-05-28 22:50:07 -0400

    ensure date in future

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
-ruby '2.5.1'
+ruby '2.6.3'
 gem 'rails', '~> 5.2.2'
 gem 'puma', '~> 3.11'
 gem 'uglifier', '>= 1.3.0'
--- app/models/parser.rb
+++ app/models/parser.rb
@@ -3,12 +3,16 @@ class Parser
   class << self
     def run(string)
       place, time_string = string.split(" at ")
-      time = parse_or_default_time(time_string)
+      time = future(parse_or_default_time(time_string))
       { place: place, time: time }
     end
 
     def parse_or_default_time(time_string)
       time_string ? Time.zone.parse(time_string) : default_time
+    end
+
+    def future(date)
+      date.future?? date : 1.day.after(date)
     end
 
     def default_time
