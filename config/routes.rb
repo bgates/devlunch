1c2828191728c96d95785ee887fdc7eb81a962d9
Brian Gates <briandotgates@gmail.com>
2019-06-03 12:33:43 -0400

    restore map to mobile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+class SlackbotDeletion 
+  def matches?(request)
+    request.request_parameters[:text].match?(/cancel/)
+  end
+end
 Rails.application.routes.draw do
   root to: "invitations#index"
+  post "invitations", to: "invitations#destroy", constraints: SlackbotDeletion.new
   resources :invitations, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
